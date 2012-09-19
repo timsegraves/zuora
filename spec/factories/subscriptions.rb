b01b7784da7576d4713b752395d96dda3c1654f2
timsegraves <timsegraves@gmail.com>
2012-09-19 09:54:57 -0500

    Fixing tests

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :subscription, :class => Zuora::Objects::Subscription do
     contract_effective_date DateTime.now
-    invoice_owner_id "owner-id"
+    invoice_owner_id 1111
     sequence(:name){|n| "Example Subscription #{n}"}
     term_start_date DateTime.now
+    user_id__c 1111
   end
 end
